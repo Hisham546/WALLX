474c4759cd87f4a1683a7774e30117669bc2dc4c
Hisham <hmuhammed546@gmail.com>
2023-07-26 19:53:05 +0530

    darkmode basic implementation completed

--- src/components/other/settings.js
+++ src/components/other/settings.js
@@ -20,18 +20,18 @@ export default function  Settings({navigation}){
 
  const { theme,updateTheme } = useContext(Context);
 
-console.log(theme)
+
 
    const toggleSwitch = (val) => {
  
-   if(val === true){
-    setIsEnabled(true)
-    updateTheme('white')
+    if(val === true){
+      setIsEnabled(previousState => !previousState)
+      updateTheme('white')
 
-  }else{
-    setIsEnabled(false)
-    updateTheme('dark')
-  }
+    }else{
+      setIsEnabled(previousState => !previousState)
+      updateTheme('dark')
+    }
    };
 
 
@@ -44,17 +44,17 @@ return(
 
 
 <View style={[styles.mainContainer,{  backgroundColor :theme === 'white' ? 'white' : '#080202' }]}>
-       <View style={styles.HeaderView}>
+       <View style={[styles.HeaderView,{  backgroundColor :theme === 'white' ? 'white' : '#080202' }]}>
            <TouchableOpacity style={{marginLeft:wp('2')}}  onPress={() => navigation.navigate('Dashboard')}>
-              <MaterialIcon name="arrow-left" size={hp('3.20%')} color="white" style={{marginRight: wp('3')}} />
+              <MaterialIcon name="arrow-left" size={hp('3.20%')}  style={{marginRight: wp('3'),color :theme === 'white' ? 'black' : 'white',}} />
            </TouchableOpacity>
-             <Text style={{color:'white',fontSize:hp('2'),marginLeft:wp('8'),fontFamily:'Manrope-Bold'}}>Settings</Text>
+             <Text style={{color :theme === 'white' ? 'black' : 'white',fontSize:hp('2'),marginLeft:wp('8'),fontFamily:'Manrope-Bold'}}>Settings</Text>
        </View>
        <View style ={styles.contentView}>
-         <Text style={{color:'white',fontSize:hp('1.8'),marginLeft:wp('5'),marginTop:hp('1'),fontFamily:'Manrope-Regular'}}>Appearance</Text>
+         <Text style={{color :theme === 'white' ? 'black' : 'white',fontSize:hp('1.6'),marginLeft:wp('5'),marginTop:hp('1'),fontFamily:'Manrope-Regular'}}>Appearance</Text>
 
          <View style ={styles.featuresView}>
-                  <Text style={{color:'white',fontSize:hp('1.8'),marginLeft:wp('5'),marginTop:hp('1'),fontFamily:'Manrope-Regular'}}>Dark Mode</Text>
+                  <Text style={{color :theme === 'white' ? 'black' : 'white',fontSize:hp('1.8'),marginLeft:wp('5'),marginTop:hp('1'),fontFamily:'Manrope-Regular'}}>Dark Mode</Text>
 
               <Switch style={styles.switchRight}
                      trackColor={{ false: "silver", true: "#A0C49D" }}
@@ -79,14 +79,12 @@ const styles= StyleSheet.create({
 
     mainContainer:{
         flex:1,
-//        backgroundColor :'#080202'
      },
 
    HeaderView:{
     width:wp('100'),
     height:hp('8'),
     flexDirection:'row',
-    backgroundColor:'#080202',
     alignItems:'center'
 
    },
